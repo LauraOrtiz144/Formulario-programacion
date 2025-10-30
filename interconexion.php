c66ae7d7af2c02e17ec81ab548139e4e300b5412
LauraOrtiz144 <laortizd@itc.edu.co>
2025-10-30 15:26:07 -0500

    Update interconexion.php

--- interconexion.php
+++ interconexion.php
@@ -2,24 +2,24 @@
 include ("conexion.php");
 require_once __DIR__ . '/conexion.php';
 if(isset($_REQUEST['btn'])){
-$nombre = $_REQUEST['name'];
+$nombre = $_REQUEST['nombre'];
 $correo = $_REQUEST['email'];  
-$telefono = $_REQUEST['number'];
-$solicitud = $_REQUEST['request'];
+$telefono = $_REQUEST['numero'];
+$solicitud = $_REQUEST['solicitud'];
 $info="Nombre del cliente: $nombre <br>
 Correo del cliente: $correo <br>
 Telefono del cliente: $telefono <br>
 <br> Solicitud del cliente: $solicitud";
 echo $info; 
 }
-$nombre = $_REQUEST['name'];
+$nombre = $_REQUEST['nombre'];
 $correo = $_REQUEST['email'];  
-$telefono = $_REQUEST['number'];
-$solicitud = $_REQUEST['request'];
+$telefono = $_REQUEST['numero'];
+$solicitud = $_REQUEST['solicitud'];
 $sql = "INSERT INTO contacto (nombre, correo, telefono, solicitud) VALUES ('$nombre', '$correo', '$telefono', '$solicitud')";
 $ejecutar =mysqli_query($conn, $sql);
 
 if (!$ejecutar) {
-    die("Error al insertar los datos: " . mysqli_error());
+    die("Error al insertar datos: " . mysqli_error());
 }    
 ?>
